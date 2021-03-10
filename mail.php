3d81413de145159c853b064c946b5bc6ced05f97
Egor Kharlamov <egorkharlamov070793@gmail.com>
2021-03-10 16:32:05 +0700

    add mail.php, but dont testing sendmail function

--- mail.php
+++ mail.php
@@ -1,2 +1,14 @@
  <?php
-    
+    $name = $_POST['name'];
+    $secondName = $_POST['secondName'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $email = $_POST['email'];
+
+
+    if (mail("egorkharlamov070793@gmail.ru", "Форма с сайта-визитки", "Имя:".$name.". E-mail: ".$email, "Фамилия:".$secondName, "Телефон:".$phone, "Сообщение:".$message ,"From: example2@mail.ru \r\n"))
+        {
+            echo "сообщение успешно отправлено";
+        } else {
+            echo "при отправке сообщения возникли ошибки";
+        }
